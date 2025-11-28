b7417f7a641db75d28aff498f01375194cdb1401
Pierre <lubitzsch@tu-berlin.de>
2025-11-28 11:35:29 +0100

    do not eval baselines here

--- eval_original_models.py
+++ eval_original_models.py
@@ -542,7 +542,7 @@ def main():
     # Evaluate each model
     all_results = []
     # Models to exclude from evaluation
-    excluded_models = ['ease', 'spectralcf']
+    excluded_models = ['ease', 'spectralcf', 'pop', 'itemknn', 'sknn']
 
     for i, model_info in enumerate(models, 1):
         # Skip excluded models
